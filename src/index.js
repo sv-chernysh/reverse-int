97649030808e19e81d40d52170b43677fd7dcbd3
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-22 13:56:00 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = String(n).split('').reverse().join('');
+    result = result.includes('-') ? result.slice(0, -1) : result;
+    return Number(result);
+};
